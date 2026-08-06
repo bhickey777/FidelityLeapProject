667c6198f6c91a812fb4408060f7baf5c66b65e4
Brian Hickey <bhickey777@gmail.com>
2026-08-06 09:54:32 -0500

    feat:add farewell message to startup output

--- starter/src/main/java/com/fidelity/leap/Main.java
+++ starter/src/main/java/com/fidelity/leap/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Greeter greeter = new Greeter();
         System.out.println(greeter.greet("Sprint 11"));
         System.out.println("Container is up and running for the skeleton. Sleeping so you can docker ps / docker logs / docker exec into it.");
+        System.out.println("So long farewell, goodbye, adios!!");
         Thread.sleep(600_000);
     }
 }
